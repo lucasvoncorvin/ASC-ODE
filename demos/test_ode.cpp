1d417a89f2f3b4ab6d9572a8e3b88f0032fad814
e12450726 <e12450726@student.tuwien.ac.at>
2025-11-20 15:21:31 +0100

    Rungekutta

--- demos/test_ode.cpp
+++ demos/test_ode.cpp
@@ -43,32 +43,15 @@ int main()
 
   Vector<> y = { 1, 0 };  // initializer list
   auto rhs = std::make_shared<MassSpring>(1.0, 1.0);
-<<<<<<< HEAD
   
-  ImplicitEuler stepper(rhs);
-=======
-
-
-
-/*
-  Vector<> Radau(3), RadauWeight(3);
-  GaussRadau (Radau, RadauWeight);
-  // not sure about weights, comput them via ComputeABfromC
-  cout << "Radau = " << Radau << ", weight = " << RadauWeight <<  endl;
-        Vector<> Gauss2c(2), Gauss3c(3);
-*/
- 
-
-  // ExplicitEuler stepper(rhs);
   // ImplicitEuler stepper(rhs);
->>>>>>> upstream/RungeKutta
 
   // RungeKutta stepper(rhs, Gauss2a, Gauss2b, Gauss2c);
 
   // Gauss3c .. points tabulated, compute a,b:
   auto [Gauss3a,Gauss3b] = ComputeABfromC (Gauss3c);
   ImplicitRungeKutta stepper(rhs, Gauss3a, Gauss3b, Gauss3c);
-
+ 
 
   /*
   // arbitrary order Gauss-Legendre
